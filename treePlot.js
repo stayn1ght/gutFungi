986832231747b33f1cf9987d16f4443e20616f58
WangM <rhongomyniadl@163.com>
2022-09-06 15:32:04 +0800

    Update treePlot.js
    数据需要用nginx托管

--- treePlot.js
+++ treePlot.js
@@ -14,7 +14,7 @@ myChart.showLoading();
 使用jQuery获取本地文件出现跨域问题，对浏览器进行设置 属性 - 目标 - 添加 --allow-file-access-from-files
 这项设置可能带来安全性问题 */
 
-/* var data = $.ajax({
+var data = $.ajax({
     url: './Gut_Fungi.json',
     type: 'get',
     dataType: 'json',
@@ -23,11 +23,9 @@ myChart.showLoading();
         return data;
     }
 }).responseJSON;
- */
 
-var data = fetch('https://github.com/stayn1ght/gutFungi/blob/main/Gut_Fungi.json')
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+
+
 myChart.hideLoading();
 myChart.setOption(
     (option = {
